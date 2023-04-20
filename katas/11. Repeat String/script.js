d4eab4d915aa6f34c6c0350dddb28bf20102c228
Davor Culibrk <davor.culibrk@krekeny.com>
2023-04-20 18:23:22 +0200

    adds repeat string kata

---
+++ katas/11. Repeat String/script.js
@@ -0,0 +1,12 @@
+// Write a function that accepts an integer n and a string s
+// as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatString(n, str) {
+  return str.repeat(n);
+}
+
+console.log(repeatString(5, "Ah"));
